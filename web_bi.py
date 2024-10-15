9081f1fa4478e3826ec6148f5b1430dd882c68eb
Carlos CØz <87679330+CCozd@users.noreply.github.com>
2024-10-15 18:09:24 -0500

    Update web_bi.py

--- web_bi.py
+++ web_bi.py
@@ -6,13 +6,13 @@ st.title('Filtro de Tableros Power BI')
 # Opciones de filtro
 opcion = st.selectbox(
     'Selecciona un tablero para visualizar:',
-    ('Tablero 1', 'Tablero 2', 'Tablero 3')
+    ('Comitee_Arrival', 'SIG_ARRIBO', 'PITAHAYA')
 )
 
 # Mostrar el tablero correspondiente según la opción seleccionada
-if opcion == 'Tablero 1':
+if opcion == 'Comitee_Arrival':
     st.components.v1.iframe("https://app.powerbi.com/view?r=eyJrIjoiOWNlODdmMmYtNGM3Ny00ZmM3LWIyNzQtM2JjNDc2NjIzMzZkIiwidCI6ImM4ODRjYzQyLTViMjEtNDA3Mi04YjdhLWE2M2QwYTZmNWQ3OSIsImMiOjR9", height=600)
-elif opcion == 'Tablero 2':
+elif opcion == 'SIG_ARRIBO':
     st.components.v1.iframe("https://app.powerbi.com/view?r=eyJrIjoiMTI4MDhmMzAtZjJjYy00YzgzLWFkODAtYjA5MDYxZjllYjEzIiwidCI6ImM4ODRjYzQyLTViMjEtNDA3Mi04YjdhLWE2M2QwYTZmNWQ3OSIsImMiOjR9", height=600)
-elif opcion == 'Tablero 3':
+elif opcion == 'PITAHAYA':
     st.components.v1.iframe("https://app.powerbi.com/view?r=eyJrIjoiMDY5YjhlOGYtMzhkMC00ZWZlLTllYmMtNDFhZTg5ODU3NTFmIiwidCI6ImM4ODRjYzQyLTViMjEtNDA3Mi04YjdhLWE2M2QwYTZmNWQ3OSIsImMiOjR9", height=600)
